cf107bec4abfb6568c0ce7420ea16fa90025f6c7
botsyrio <df1593@nyu.edu>
2017-11-29 19:32:35 -0500

    uh oh

--- pagin.java
+++ pagin.java
@@ -1,7 +1,7 @@
 package pagingLab;
 import java.util.*;
 import java.io.*;
-public class Pagin {
+public class pagin {
 	public static void main (String[] args){
 		int machineSize = Integer.valueOf(args[0]);
 		int pageSize = Integer.valueOf(args[1]);
@@ -31,24 +31,24 @@ public class Pagin {
 		process processList[];
 		if(J==1){
 			processList = new process[2];
-			processList[1] = new process(1, 1.0, 0.0, 0.0, 111%processSize);
+			processList[1] = new process(1, 1.0, 0.0, 0.0, -1, numRefs);
 		}
 		else
 			processList = new process[5];
 		
 		if(J==2){
 			for(int i =1; i<5; i++)
-				processList[i] = new process(i, 1.0, 0.0, 0.0, (111*i)%processSize);
+				processList[i] = new process(i, 1.0, 0.0, 0.0, -1, numRefs);
 		}
 		if(J==3){
 			for(int i =1; i<5; i++)
-				processList[i] = new process(i, 0.0, 0.0, 0.0, (111*i)%processSize);
+				processList[i] = new process(i, 0.0, 0.0, 0.0, -1, numRefs);
 		}
 		if(J==4){
-			processList[1] = new process(1, 0.75, 0.25, 0.0, (111)%processSize);
-			processList[2] = new process(2, 0.75, 0.0, 0.25, (111*2)%processSize);
-			processList[3] = new process(3, 0.75, 0.125, 0.125, (111*3)%processSize);
-			processList[4] = new process(4, 0.5, 0.125, 0.125, (111*4)%processSize);
+			processList[1] = new process(1, 0.75, 0.25, 0.0, -1, numRefs);
+			processList[2] = new process(2, 0.75, 0.0, 0.25, -1, numRefs);
+			processList[3] = new process(3, 0.75, 0.125, 0.125, -1, numRefs);
+			processList[4] = new process(4, 0.5, 0.125, 0.125, -1, numRefs);
 		}
 		
 		frame frameTable[] = new frame[machineSize/pageSize];
@@ -59,31 +59,146 @@ public class Pagin {
 		
 		int currProcess = 1;
 		processList[1].quant = q;
-		
-		for(int t=0; t<(processList.length-1)*numRefs; t++){	
-			if(processList[currProcess].quant==0){
-				if(currProcess==processList.length-1)
-					currProcess = 1;
+		//int totalResidency=0;
+		int t=1;
+		while(t<(processList.length-1)*numRefs+1){
+			if(processList[currProcess].quant==0||processList[currProcess].refsLeft==0){
+				int candidate =currProcess;
+				if(candidate==processList.length-1)
+					candidate = 1;
 				else
-					currProcess++;
+					candidate++;
+				while(processList[candidate].refsLeft==0){
+					if(candidate==processList.length-1)
+						candidate = 1;
+					else
+						candidate++;
+				}
+				currProcess = candidate;
 				processList[currProcess].quant=q;
 			}
-			int tmp = processList[currProcess].ref;
-			double y = rand.nextDouble()/Integer.MAX_VALUE+1d;
 			
-			if(y<processList[currProcess].a)
-				processList[currProcess].ref = (tmp+1+processSize)%processSize;
-			else if(y<processList[currProcess].a+processList[currProcess].b)
-				processList[currProcess].ref = (tmp-5+processSize)%processSize;
-			else if(y<processList[currProcess].a+processList[currProcess].b+processList[currProcess].c)
-				processList[currProcess].ref = (tmp+4+processSize)%processSize;
-			else
-				processList[currProcess].ref = (rand.nextInt()*(processSize-1))%processSize;
-			
-			
+			if(processList[currProcess].ref==-1){
+				processList[currProcess].ref = 111*currProcess%processSize;
+			}
+			else{
+				int tmp = processList[currProcess].ref;
+				double y = rand.nextInt();
+				System.out.println(y);
+				y=y/2147483648.0;
+				
+				if(y<processList[currProcess].a)
+					processList[currProcess].ref = (tmp+1+processSize)%processSize;
+				else if(y<processList[currProcess].a+processList[currProcess].b){
+					processList[currProcess].ref = (tmp-5+processSize*10)%processSize;
+					//System.out.println("b");
+				}
+				else if(y<processList[currProcess].a+processList[currProcess].b+processList[currProcess].c){
+					processList[currProcess].ref = (tmp+4+processSize)%processSize;
+					//System.out.println("c");
+				}
+				else
+					processList[currProcess].ref = rand.nextInt()%processSize;
+					//System.out.println("d");
+			}
+			System.out.println(currProcess+" references word "+processList[currProcess].ref+" at time "+(t));
+						
+			int page = processList[currProcess].ref/pageSize;
+			boolean inThere = false;
+			for(int i=frameTable.length-1; i>=0; i--){
+				if(!frameTable[i].filled && !inThere && !(frameTable[i].process==currProcess && frameTable[i].pageNumber==page)){
+					frameTable[i].filled=true;
+					frameTable[i].process = currProcess;
+					frameTable[i].pageNumber=page;
+					frameTable[i].startTime = t;
+					frameTable[i].tLastUsed = t;
+					System.out.println("process "+currProcess+ " page "+page+" placed in frame "+i);
+
+					processList[frameTable[i].process].numFaults++;
+					inThere = true;
+				}
+				else if(frameTable[i].process==currProcess && frameTable[i].pageNumber==page){
+					inThere = true;
+					frameTable[i].tLastUsed=t;
+				}
+			}
+			if(!inThere){
+				processList[currProcess].numFaults++;
+				if(r.equals("lru")){
+					int leastRecent=0;
+					for(int frameNum=1; frameNum<frameTable.length; frameNum++){
+						if(frameTable[frameNum].tLastUsed<frameTable[leastRecent].tLastUsed)
+							leastRecent = frameNum;
+					}//end for
+					processList[frameTable[leastRecent].process].residency+=(t-frameTable[leastRecent].startTime);
+					processList[frameTable[leastRecent].process].numEvictions++;
+					//totalResidency+=t-frameTable[leastRecent].startTime;
+					System.out.println("process "+frameTable[leastRecent].process+ " page "+frameTable[leastRecent].pageNumber+" evicted from frame "+leastRecent);
+					
+					frameTable[leastRecent].process = currProcess;
+					frameTable[leastRecent].pageNumber=page;
+					frameTable[leastRecent].startTime = t;
+					frameTable[leastRecent].tLastUsed = t;
+				}//end lru procedure
+				if(r.equals("lifo")){
+					int lastIn=0;
+					for(int frameNum=1; frameNum<frameTable.length; frameNum++){
+						if(frameTable[frameNum].startTime>frameTable[lastIn].startTime)
+							lastIn = frameNum;
+					}//end for
+					processList[frameTable[lastIn].process].residency+=(t-frameTable[lastIn].startTime);
+					processList[frameTable[lastIn].process].numEvictions++;
+					//totalResidency+=t-frameTable[leastRecent].startTime;
+					System.out.println("process "+frameTable[lastIn].process+ " page "+frameTable[lastIn].pageNumber+" evicted from frame "+lastIn);
+					
+					frameTable[lastIn].process = currProcess;
+					frameTable[lastIn].pageNumber=page;
+					frameTable[lastIn].startTime = t;
+					frameTable[lastIn].tLastUsed = t;					
+				}
+				if(r.equals("random")){
+					int random = rand.nextInt();
+					System.out.println(random);
+					random = random%frameTable.length;
+					processList[frameTable[random].process].residency+=(t-frameTable[random].startTime);
+					processList[frameTable[random].process].numEvictions++;
+					//totalResidency+=t-frameTable[leastRecent].startTime;
+					System.out.println("process "+frameTable[random].process+ " page "+frameTable[random].pageNumber+" evicted from frame "+random);
+					
+					frameTable[random].process = currProcess;
+					frameTable[random].pageNumber=page;
+					frameTable[random].startTime = t;
+					frameTable[random].tLastUsed = t;	
+				}
+			}
+			processList[currProcess].refsLeft--;
 			processList[currProcess].quant--;
+			t++;
 		}
-		
+		/*for(int i=0; i<frameTable.length; i++){
+			//processList[frameTable[i].process].residency+=t-frameTable[i].startTime;
+			totalResidency+=t-frameTable[i].startTime-1;
+		}*/
+		rand.close();
+		System.out.println("The machine size is "+machineSize);
+		System.out.println("The page size is "+pageSize);
+		System.out.println("The process size is "+processSize);
+		System.out.println("The job mix number is "+J);
+		System.out.println("The number of references per process is "+numRefs);
+		System.out.println("The replacement algorithm is "+r);
+		System.out.println();
+		int totalFaults = 0;
+		int totalResidency=0;
+		int totalEvictions=0;
+		for(int i=1; i<processList.length; i++){
+			totalFaults+=processList[i].numFaults; 
+			totalResidency+=processList[i].residency;
+			totalEvictions += processList[i].numEvictions;
+			double avgResidency = ((double)processList[i].residency)/((double)(processList[i].numEvictions));
+			System.out.println("Process "+i+" had "+processList[i].numFaults+" faults and "+avgResidency+" average residency");
+		}
+		System.out.println();
+		System.out.println("The total number of faults is "+totalFaults+" and the overall average residency is "+((double)totalResidency)/((double)totalEvictions));
 	}
 }
 class process{
@@ -93,14 +208,22 @@ class process{
 	double c;
 	int quant;
 	int ref;
+	int numFaults;
+	int numEvictions;
+	int residency;
+	int refsLeft;
 	
-	process(int p, double A, double B, double C, int reference){
+	process(int p, double A, double B, double C, int reference, int rLeft){
 		pNumber = p;
 		a=A;
 		b=B;
 		c=C;
 		quant=0;
 		ref = reference;
+		residency = 0;
+		numFaults = 0;
+		numEvictions = 0;
+		refsLeft = rLeft;
 	}
 }
 
@@ -108,5 +231,6 @@ class frame{
 	int process;
 	int pageNumber;
 	int startTime;
+	int tLastUsed;
 	boolean filled;
 }
